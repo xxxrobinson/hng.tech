a31c9a9af13f8d16322927b17a90c63012109c49
uggoprince <uggoprince@gmail.com>
2019-04-03 19:18:35 +0100

    done adding my profile

--- index.php
+++ index.php
@@ -56,6 +56,9 @@
      case '/interns/hackthievist' :
           require __DIR__ . '/views/hackthievist.php';
           break;
+     case '/interns/uggoprince' :
+          require __DIR__ . '/views/uggoprince.php';
+          break;
       default:
           require __DIR__ . '/views/404.php';
           break;
