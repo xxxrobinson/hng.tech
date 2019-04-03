b5e21c17d8373300df4b8bc9f4acce88670a2118
Igboanyika Nnaemeka Robin <emeka14real@gmail.com>
2019-04-03 13:10:14 -0700

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $smtp_username = 'c2b694593e436b';
 $smtp_password = '0663aac7006359';
 $smtp_port = 2525;
 
-// stripe configuration var
+// stripe configuration Var
 $stripe_secret_key = "sk_test_2DXp9R4OyZp65DDaJRLVb2mF";
 
 
