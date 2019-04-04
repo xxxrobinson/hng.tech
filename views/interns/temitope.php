5ba5df115d761d2b21244d8b38dd0ff2cb0ef6ea
Temitope Olutade <topeolutade@ymail.com>
2019-04-04 09:09:40 +0100

    Updated my profile

--- views/interns/temitope.php
+++ views/interns/temitope.php
@@ -12,7 +12,7 @@
                   <div class="col-md-6 col-xs-12">
                      <div class="intern-block border-blue">
                         <img src="https://res.cloudinary.com/topzy/image/upload/v1554328504/tope.jpg" alt="Temitope Olutade">
-                        <h6>Temitope Olutade</h6>
+                        <h6>Temitope.O.Olutade</h6>
                         <p>Front-end Engineer</p>
                         <ul class="list">
                            <li><a href="#" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
@@ -24,7 +24,7 @@
                   <div class="col-md-6 col-xs-12 align-items-center">
                      <div class="intern-bio border-blue">
                         <h6>About me</h6>
-                        <p> My name is Temitope Olutade , enthusiast in programming , i am open to learning to advance my programming knowledge</p>
+                        <p> My name is Temitope Olutade , enthusiast in programming and technology , i am open to learning to advance my programming knowledge..I love creativity</p>
                         <br />
                         <h6>Skills</h6>
                         <p>JavaScript | C# | JAVA</p>
