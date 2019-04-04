eb98d29b1411eb4fdf228de5118849c25ed50a71
olaysco <olayiwolaodunsi@gmail.com>
2019-04-04 07:48:53 +0100

    profile correction

--- views/interns/function.php
+++ views/interns/function.php
@@ -11,23 +11,23 @@
                <div class="col-md-12 col-sm-6 col-xs-12">
                   <div class="col-md-6 col-xs-12">
                      <div class="intern-block border-blue">
-                        <img src="https://res.cloudinary.com/iambeejayayo/image/upload/v1547954566/fav-500.png" alt="Mark Essien">
-                        <h6>Mark Essien</h6>
-                        <p>Front-end Engineer</p>
+                        <img src="https://res.cloudinary.com/function/image/upload/v1554314551/20190307_161259.jpg" alt="Olayiwola Odunsi">
+                        <h6>Olayiwola Odunsi</h6>
+                        <p>Back-end Engineer</p>
                         <ul class="list">
-                           <li><a href="#" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
-                           <li><a href="#" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
-                           <li><a href="#" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                           <li><a href="https://twitter.com/horllaysco" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/olaysco" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@olayiwolaodunsi" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
                         </ul>
                      </div>
                   </div>
                   <div class="col-md-6 col-xs-12 align-items-center">
                      <div class="intern-bio border-blue">
                         <h6>About me</h6>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dicta, ipsam aliquam fugit voluptatum ad. Nihil sit ipsum consectetur nulla? Nesciunt illum mollitia repellat corrupti deleniti recusandae, repellendus eaque exercitationem?lore</p>
+                        <p>A software engineer with four (4) years programming experience in full stack development using PHP/LARAVEL/CSS/JS having worked on several web application projects as a freelance developer of which personal ones are hosted on my Github profile github.com/olaysco.</p>
                         <br />
                         <h6>Skills</h6>
-                        <p>JavaScript | PHP | Python</p>
+                        <p>PHP | JAVA | Python</p>
                      </div>
                   </div>
                </div>
