6a262a6c80774e79bddd37f5c1cc9c400605c6e3
Mr-Malomz <malomo.alaba@yahoo.com>
2019-04-04 07:41:46 +0100

    Demola's Updated Profile

---
+++ views/interns/DemolaM.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/dtgbzmpca/image/upload/v1554309203/Malomo_Ademola.jpg" alt="Demola Malomo">
+                        <h6>Demola Malomo</h6>
+                        <p>Front-end Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/malomz" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/Mr-Malomz" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@demlabz" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Hi, I'm Demola, a certified SAP Materials Management Associate. I recently fell in love with web development and it has been an awesome experience.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | Python | ABAP</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
