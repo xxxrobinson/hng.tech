e9132070d9ea5079473668b36987214da5e46a7c
prince-curie <prince.chibuike@gmail.com>
2019-04-04 04:17:06 +0100

    lets add it

--- views/interns/dmatrix.php
+++ views/interns/dmatrix.php
@@ -11,26 +11,23 @@
                <div class="col-md-12 col-sm-6 col-xs-12">
                   <div class="col-md-6 col-xs-12">
                      <div class="intern-block border-blue">
-                        <img src="https://res.cloudinary.com/prince-curie/image/upload/c_crop,x_01,y_01/v1519776859/rsz_img_20180116_121815_wqutel_dovfsy.jpg" alt="image of Onwubiko Chibuike">
-                        <h6>Onwubiko Chibuike</h6>
-                        <p>Software Developer</p>
+                        <img src="https://res.cloudinary.com/domvwrihz/image/upload/c_scale,h_300,w_300/v1554324727/_MG_2721.jpg" alt="Elijah Okokon">
+                        <h6>Elijah Okokon</h6>
+                        <p>Full stack Developer</p>
                         <ul class="list">
-                           <li><a href="https://twitter.com/Thisischibuike" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
-                           <li><a href="https://github.com/prince-curie" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
-                           <li><a href="https://medium.com/@princechibuike" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                           <li><a href="https://twitter.com/facebook" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/DmatrixNg" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@okoelijah" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
                         </ul>
                      </div>
                   </div>
                   <div class="col-md-6 col-xs-12 align-items-center">
                      <div class="intern-bio border-blue">
                         <h6>About me</h6>
-                        <p>A budding software developer, 
-                        in love with building things. 
-                        I write JavaScript and nodeJS. 
-                        Will be glad to interact with you.</p>
+                        <p> I am a full stack Developer, interested in creating tech solutions </p>
                         <br />
                         <h6>Skills</h6>
-                        <p>JavaScript | CSS | nodeJS</p>
+                        <p>Html | PHP | C++  </p>
                      </div>
                   </div>
                </div>
