8e083c5d3b1a5a56be497058127f673be5478180
thejsdev <uchenoeldike@gmail.com>
2019-04-04 21:42:17 +0100

    added thejsdev.php

---
+++ views/interns/thejsdev.php
@@ -0,0 +1,36 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/thejsdev/image/upload/v1554384437/htsng.jpg" alt="Uche">
+                        <h6>Uchechukwu Nuel</h6>
+                        <p>Javascript Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/noelukwa" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/thejsdev" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@javascriptar" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I am currently studying computer science at the university of benin, outside my daily school activities, i am  also currently enrolled in udacity fullstack nano degree to broaden  my skills and knowledge. I am also a freelance web developer at freelancer.com where i have worked on numerous projects ranging from simple front end fixes to backend programming.</p>
+                        <h6>Skills</h6>
+                        <p>Mongo Db| React.js| Node.js</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
