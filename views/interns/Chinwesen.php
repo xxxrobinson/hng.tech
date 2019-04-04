d50f4c906db9e1ce962e7cf85aecc0e2c7bdb8b9
Chinwesen <49123979+Chinwesen@users.noreply.github.com>
2019-04-04 21:24:22 +0100

    Add My Profile

---
+++ views/interns/Chinwesen.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/chinwesen/image/upload/v1554320908/chinwesen.png" alt="Ejuh Chinweikpe">
+                        <h6>Ejuh Chinweikpe</h6>
+                        <p>Front-end Engineer</p>
+                        <ul class="list">
+                           <li><a href="http://twitter.com/ChinweEjuh" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/chinwesen" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@chinwesen" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>My name is Ejuh Chinweikpe, I was born in Nigeria. I am ambitious and driven. I thrive on challenge and constantly set goals for myself, so I have something to strive toward. I’m not comfortable with settling, and I’m always looking for an opportunity to do better and achieve greatness.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | PHP</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
