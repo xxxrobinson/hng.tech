2645937cb8c631bc924ee34e05add103bbceb365
Presh <p_webs>
2019-04-04 01:54:19 +0100

    latest update

---
+++ views/interns/presh.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/presh538/image/upload/v1554337352/IMG_20181011_225904.jpg" alt="Precious Ogar">
+                        <h6>Precious Ogar</h6>
+                        <p>UI/IX Designer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/thepresh_legacy" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/Presh538" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@ogarpre" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I'm a dilligent and fast learner, and ready for team-work. Skilled in product design and front-end development and also a budding app developer</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>HTML | CSS | Flutter</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
