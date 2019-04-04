3afea1d6d1201dee53b1afaaceb6af51a1f20866
Iwakin Omotoye <iwakinomotoye@gmail.com>
2019-04-04 20:48:58 +0100

    profile styling updated

---
+++ views/interns/iwakinomotoye.php
@@ -0,0 +1,45 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12" 
+               style="border-left: 5px solid black; border-right: 5px solid black; box-shadow: 0px 0px 5px rgba(0,0,0,0.2); margin: 0 0 30px 0;">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue" style='border-right: 1px solid #ddd; padding: 20px 0 0 0;'>
+                        <img src="https://res.cloudinary.com/iwakinomotoye/image/upload/v1554351669/samples/IMG_20181031_125757.jpg" 
+                        style="padding-top: 100px 0; border-radius: 200px;" alt="Iwakin Omotoye">
+                        <div style='display: inline-block; position:relative; bottom:80px; left:120; padding: 0 20px; color: white; background-color: rgba(0,0,0,0.5);'>
+                        <h2  style='margin:0;'>Iwakin Omotoye</h2>
+                        <h4 style='margin:0;'>Front-End Developer</h4>
+                        </div>
+                        
+                        <ul class="list" style='list-style: none; margin-left: 100px;'>
+                           <li><a style="float:left; padding: 0 20px; border-right: 1px solid #ddd;" href="https://twitter.com/Iwakintoye" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a style="float:left; padding: 0 20px; border-right: 1px solid #ddd;" href="https://github.com/Iwakinomotoye" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a style="padding: 0 20px;" href="https://medium.com/@iwakinomotoye" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center" style="padding: 50px 0;">
+                     <div class="intern-bio border-blue">
+                        <h2 style=" text-align: center; color: goldenrod; text-decoration:underline;">About me</h2>
+                        <p style="font-size: 18px;">I am a civil engineer by certificate, but a self taught programmer. I really love solving problems, I do not rest until I get problems
+                        solved. I also believe in solving arbitrary problems my own way first, then learn where I make mistakes from others.</p>
+                        <br />
+                        <h3 style="text-align:center; color: goldenrod; text-decoration: underline;">Skills</h3>
+                        <p style="background-color: rgba(0,0,0,0.1); text-align:center; font-size: 18px; box-shadow:0 0 5px rgba(0,0,0,0.2);">HTML </p>
+                        <p style="background-color: rgba(0,0,0,0.1); text-align:center; font-size: 18px; box-shadow:0 0 5px rgba(0,0,0,0.2);">CSS </p>
+                        <p style="background-color: rgba(0,0,0,0.1); text-align:center; font-size: 18px; box-shadow:0 0 5px rgba(0,0,0,0.2);">JAVASCRIPT </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
