2c4a519bef4197b2b6942af8034dae0b2d187203
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-04-04 16:17:37 +0100

    Add my profile

---
+++ views/interns/aviator.php
@@ -0,0 +1,38 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/dr7ml6o9h/image/upload/v1554387738/Avia.jpg" alt="Aviator">
+                        <h6> Onyechere Kingsolomon Ifeanyi </h6>
+                        <p> Backend Engineer </p>
+                        <ul class="list">
+                           <li><a href="https://www.twitter.com/AviatorLoper" target="_blank"> <i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://www.github.com/chinedumIfeanyi" target="_blank"> <i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://www.medium.com/@AviatorLoper" target="_blank"> <i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p> I am a javascript and php web developer. I love the work i do and the problems i solve with web technologies because it does not only challenges me, it also offers me the opportunity to satisfy my curiousity and stretch my imagination. 
+                        </p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p> Javascript | PHP | Database </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
