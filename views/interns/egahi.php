9279aa068d3b44e2f15287409e92263184a067fd
Egahi <okworisamuel6@gmail.com>
2019-04-04 06:04:38 +0100

    added egahi's profile page

---
+++ views/interns/egahi.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/egahi/image/upload/v1554352620/Screenshot_20190115-225020_Gallery.png" alt="Egahi">
+                        <h6>Samuel Egahi Okwori</h6>
+                        <p>Back-end Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/egahi_so" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/egahi" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@egahi_so" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I am a .NET developer (with skills skewed towards back-end), I love philosophical debates and I occasionally get high on music.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>.NET | C# | MS SQL Server</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
