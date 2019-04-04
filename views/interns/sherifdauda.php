260e48df0b869b76cb8cfb665680c82f336f189c
[sherifdauda] <[sherifdauda8@gmail.com]>
2019-04-04 06:58:25 +0100

    Add sherifdauda.php to interns

---
+++ views/interns/sherifdauda.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/sherifdauda/image/upload/v1554308262/sherif.jpg" alt="Sherif Dauda">
+                        <h6>Sherif Dauda</h6>
+                        <p>UI/UX Designer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/sherifdauda_" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/sherifdauda" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@sherifdauda" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I am a designer who is well versed in User Interface design, 3D Modeling, Vector Design, Illustration, Video Editing and 2D Motion Graphics. I have worked with programmers to design a health care platform. I have 2 years of design experience.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>Visual Design | Design Thinking | Motion Graphics</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
