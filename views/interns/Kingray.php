44493385701a9da46961c716abe9589795ca23ef
daraygreat8 <programmerdarhaey@gmail.com>
2019-04-04 07:39:40 +0100

    Created my profile

---
+++ views/interns/Kingray.php
@@ -0,0 +1,39 @@
+
+
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/kingray/image/upload/c_scale,w_100/v1554354470/samples/damilare.jpg" alt="Mark Essien">
+                        <h6>Oyewale Damilare</h6>
+                        <p>Front-end Engineer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/oluwa_damilare_" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/daraygreat8/" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@damifreelance" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>Front-End Developer</h6>
+                        <p>I'm a developer who is keen on building impressive, lasting, and impactful solutions. I'm currently on the path to becoming a software developer as I have plans of changing the world with my coding skills</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | HTML | CSS</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
