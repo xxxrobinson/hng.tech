2b3fa5c6da2c211b6d01bf16552562abf55dd76a
olanetsoft <heedris2olubisi@gmail.com>
2019-04-04 10:55:33 +0100

    my olanetsoft profile created

---
+++ views/interns/olanetsoft.php
@@ -0,0 +1,38 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/olanetsoft/image/upload/v1554336660/38702099_1723141854450338_3022484007930560512_n.png" alt="Mark Essien">
+                        <h6>Olubisi Idris Ayinde</h6>
+                        <p>Aspiring Full stack Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/olanetsoft" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/Olanetsoft" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@heedris2olubisi/bug-detected-on-website-ee8ea58287d" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I am Olubisi Idris a graduate of Electrical and Electronics Engineering. I am also a self taught Front End developer but i am aspiring to be a full stak developer.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | HTML | CSS</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
+
